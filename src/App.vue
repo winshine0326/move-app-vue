75905750fc055719ccef54fdc2188e8768ec2e30
winshine0326 <shane10340@gmail.com>
2025-07-28 14:46:55 +0900

    속성 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{ title }}</h1>
+  <h1 class="bg-yellow" :style="textRed">{{ title }}</h1>
   <p>개봉 : {{ year }}</p>
   <p>장르 : {{ category }}</p>
 </template>
@@ -12,9 +12,14 @@ export default {
       title: "노량",
       year: 2025,
       category: "액션, 드라마",
+      textRed: { color: "red" }, // 객체로 넘겨야함
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.bg-yellow {
+  background-color: yellow;
+}
+</style>
