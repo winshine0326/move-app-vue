4ecbdde1104ecf5473bcf4a78a0fd6579604e1d5
winshine0326 <shane10340@gmail.com>
2025-07-28 16:18:30 +0900

    class를 동적으로 다루기

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="event">
+  <div class="event" :class="{ show: isOpen }">
+    <!-- {클래스명 : 조건}-->
     <p>{{ text }}</p>
+    <button @click="isOpen = false">X</button>
   </div>
 </template>
 <script>
@@ -8,6 +10,11 @@ export default {
   name: "EventComponent",
   props: {
     text: String,
+  },
+  data() {
+    return {
+      isOpen: true,
+    };
   },
 };
 </script>
@@ -17,8 +24,20 @@ export default {
   color: #fff;
   padding: 10px 20px;
   text-align: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  display: none;
 }
 .event p {
+  margin: 0;
+}
+
+.show {
+  display: flex;
+}
+
+.event button {
   margin: 0;
 }
 </style>
