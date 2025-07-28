a0db1058dd7db079cb1ee00adad5b214ae8a4534
winshine0326 <shane10340@gmail.com>
2025-07-28 15:02:45 +0900

    click 이벤트로 좋아요 버튼 만들기

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
     <!-- 아니 속성값이 존재하지 않아도 에러가 나지 않는다고????? 혁신이다 -->
     <p>개봉 : {{ item.year }}</p>
     <p>장르 : {{ item.category }}</p>
+    <button @:click="increseLike(i)">좋아용👍</button>
+    <span>{{ item.like }}</span>
   </div>
-  <!-- <p v-for="(item, i) in foods" :key="i">{{ item }}</p> -->
 </template>
 
 <script>
@@ -14,26 +15,33 @@ export default {
   name: "App",
   data() {
     return {
-      foods: ["김밥", "순대", "만두"],
       data: [
         {
           title: "노량",
           year: 2025,
           category: "액션, 드라마",
           textRed: { color: "red" },
+          like: 0,
         },
         {
           title: "아쿠아맨과 로스트 킹덤",
           year: 2023,
           category: "액션, 판타지, 어드벤처",
+          like: 0,
         },
         {
           title: "패배히로인이 너무 많아!",
           year: 2024,
           category: "러브코미디, 학원",
+          like: 0,
         },
       ],
     };
+  },
+  methods: {
+    increseLike(i) {
+      this.data[i].like += 1;
+    },
   },
 };
 </script>
